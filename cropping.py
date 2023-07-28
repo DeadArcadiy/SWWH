fcca991f610bea57a522cf3d5c260d859fb74298
bleyzzi <dakrnal@gmail.com>
2023-07-28 13:53:38 +0300

    minor fixes

--- cropping.py
+++ cropping.py
@@ -9,17 +9,21 @@ import numpy as np
 
 
 class ImageCropper:
-    def __init__(self, path, name):
-        #        self.path = path
-        #        self.name = name
-        self.image = cv2.imread(path + name + ".JPG", 3)
-        b, g, r = cv2.split(self.image)
-        self.image = cv2.merge([r, g, b])
+    def __init__(self, path, save_path):
+        self.path = path
+        self.save_path = save_path
 
-    def rectangle_crop(self):
+    def image_maker(self, name):
+        image = cv2.imread(self.path + name + ".JPG", 3)
+        b, g, r = cv2.split(image)
+        image = cv2.merge([r, g, b])
+        return image
+
+    @staticmethod
+    def rectangle_crop(self, image):
         rectangle_cropped_image = None
-        original = self.image.copy()
-        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        original = image.copy()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
@@ -31,13 +35,12 @@ class ImageCropper:
         for c in cnts:
             x, y, weight, height = cv2.boundingRect(c)
             if weight > 500 and height > 500:
-                cv2.rectangle(self.image,
+                cv2.rectangle(image,
                               (x, y),
                               (x + weight, y + height),
                               (36, 255, 12),
                               2)
-                ROI = original[y:y + height, x:x + weight]
-                rectangle_cropped_image = ROI.copy()
+                rectangle_cropped_image = original[y:y + height, x:x + weight]
         if rectangle_cropped_image is not None:
             return rectangle_cropped_image
         else:
@@ -45,14 +48,13 @@ class ImageCropper:
 
     @staticmethod
     def circle_crop(self, rectangle_cropped_image):
-        img = rectangle_cropped_image.copy()
-        height, diametr = img.shape[:2]
-        mask1 = np.zeros_like(img)
+        height, diametr = rectangle_cropped_image.shape[:2]
+        mask1 = np.zeros_like(rectangle_cropped_image)
         mask1 = cv2.circle(mask1,
                            (height // 2, diametr // 2),
                            int(diametr / 2 - diametr / 20),
                            (255, 255, 255),
                            -1)
-        circle_cropped_image = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+        circle_cropped_image = cv2.cvtColor(rectangle_cropped_image, cv2.COLOR_BGR2BGRA)
         circle_cropped_image[:, :, 3] = mask1[:, :, 0]
         return circle_cropped_image
