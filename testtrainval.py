eb030579d22b3a334fe5050cfd79c858c0b9c637
DeadArcadiy <denisborycev@gmail.com>
2023-10-05 15:40:57 +0300

    seed added to splitter

--- testtrainval.py
+++ testtrainval.py
@@ -1,7 +1,7 @@
 import os
 import random
 import shutil
-
+random.seed(7)
 ls = os.listdir('castom-masks/img/')
 
 path = 'castom-masks/img/'
@@ -21,7 +21,7 @@ random.shuffle(ls)
 for i,file in enumerate(ls):
     if i < 30:
         shutil.copyfile(path+file,saveval+file)
-    elif i < 39:
+    elif i < 40:
         shutil.copyfile(path+file,savetest+file)
     else:
         shutil.copyfile(path+file,savetrain+file)
