273555653d791eb312e7c3989b7baeb07e035c97
Arcadiy <denisborycev@gmail.com>
2023-11-30 20:07:48 +0300

    final model

--- model.py
+++ model.py
@@ -9,9 +9,9 @@ class ModelCreator:
     #encoder x:
     def encoder_x(this,input,n_filters):
         x = tf.keras.layers.Conv2D(n_filters, 3,padding = "same", kernel_initializer = "he_normal",use_bias=False)(input)
-        x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.LeakyReLU()(x)
+        x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(x)
         return x
 
     #decoder_x:
@@ -30,21 +30,24 @@ class ModelCreator:
     def buildmodel(this):
         inputlayer = tf.keras.layers.Input(shape=(HEIGHT,HEIGHT,3))
         print(inputlayer)
-        e1 = this.encoder_x(inputlayer,48)
-        e2 = this.encoder_x(e1,48)
-        e3 = this.encoder_x(e2,176)
-        e4 = this.encoder_x(e3,416)
-        x = this.encoder_x(e4,128)
-        x = tf.keras.layers.Conv2DTranspose(128, 3, strides= 2,padding = "same", kernel_initializer = "he_normal",use_bias=False)(x)
-        x = tf.keras.layers.LeakyReLU()(x)
-        x = this.decoder_x(x,e4,416)
-        x = this.decoder_x(x,e3,176)
-        x = this.decoder_x(x,e2,48)
-        x = this.decoder_x(x,e1,48)
+        e1 = this.encoder_x(inputlayer,32)
+        e2 = this.encoder_x(e1,64)
+        e3 = this.encoder_x(e2,128)
+        e4 = this.encoder_x(e3,256)
+        e5 = this.encoder_x(e4,512)
+        x = tf.keras.layers.Conv2DTranspose(512, 3,strides=2,padding = "same", kernel_initializer = "he_normal",use_bias=False)(e5)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.ReLU()(x)
+        x = this.decoder_x(x,e4,256)
+        x = this.decoder_x(x,e3,128)
+        x = this.decoder_x(x,e2,64)
+        x = this.decoder_x(x,e1,32)
         outputlayer = this.output(x)
         print(outputlayer)
         
         unet_model = tf.keras.Model(inputlayer, outputlayer, name="U-Net")
+
+        unet_model.compile('Adam', loss=sm.losses.binary_focal_dice_loss, metrics=[sm.metrics.iou_score])
 
         return unet_model
 
@@ -53,17 +56,16 @@ class ModelCreator:
     def build_tunermodel(this,hp):
         inputlayer = tf.keras.layers.Input(shape=(HEIGHT,HEIGHT,3))
         print(inputlayer)
-        layer1 = hp.Int('layer1', min_value=8, max_value=64, step=8)
+        layer1 = hp.Int('layer1', min_value=16, max_value=64, step=16)
         e1 = this.encoder_x(inputlayer,layer1)
-        layer2 = hp.Int('layer2', min_value=32, max_value=96, step=8)
+        layer2 = hp.Int('layer2', min_value=32, max_value=96, step=32)
         e2 = this.encoder_x(e1,layer2)
-        layer3 = hp.Int('layer3', min_value=64, max_value=256, step=16)
+        layer3 = hp.Int('layer3', min_value=64, max_value=256, step=64)
         e3 = this.encoder_x(e2,layer3)
-        layer4 = hp.Int('layer4', min_value=64, max_value=512, step=32)
+        layer4 = hp.Int('layer4', min_value=128, max_value=512, step=128)
         e4 = this.encoder_x(e3,layer4)
-        layer5 = hp.Int('layer5', min_value=64, max_value=512, step=64)
-        x = this.encoder_x(e4,layer5)
-        x = tf.keras.layers.Conv2DTranspose(layer5, 3, strides= 2,padding = "same", kernel_initializer = "he_normal",use_bias=False)(x)
+        layer5 = hp.Int('layer5', min_value=256, max_value=1024, step=256)
+        x = tf.keras.layers.Conv2D(layer5, 3,padding = "same", kernel_initializer = "he_normal",use_bias=False)(e4)
         x = tf.keras.layers.ReLU()(x)
         x = this.decoder_x(x,e4,layer4)
         x = this.decoder_x(x,e3,layer3)
