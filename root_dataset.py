03703fe2eb20c2e9ef5e521d9e606ccb4f51f1e9
Denis Borychev <denisborycev@gmail.com>
2024-01-05 19:36:06 +0300

    migrate to torch

---
+++ root_dataset.py
@@ -0,0 +1,32 @@
+import torch
+import albumentations as albu
+import cv2
+from tqdm import tqdm
+import os
+
+class Root_dataset(torch.utils.data.Dataset):
+    def __init__(self,augmentations,image_folder,mask_folder,device):
+        self.image_folder = image_folder
+        self.mask_folder = mask_folder
+        self.transforms = augmentations
+        self.images = os.listdir(image_folder)
+        self.device = device
+        print(self.images)
+
+    def __getitem__(self,i):
+        image = cv2.imread(self.image_folder+'/'+self.images[i])
+        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.mask_folder+'/'+self.images[i],cv2.IMREAD_GRAYSCALE)
+        transformed = self.transforms(image=image, mask=mask)
+        transformed_image = transformed['image']
+        transformed_mask = transformed['mask']
+        transformed_mask = transformed_mask.reshape(512,512,1)
+        transformed_image = torch.Tensor(transformed_image)
+        transformed_mask = torch.Tensor(transformed_mask)
+        transformed_image = transformed_image.permute((2, 0, 1))
+        transformed_mask = transformed_mask.permute((2, 0, 1))
+        return transformed_image.to(self.device),transformed_mask.to(self.device)
+            
+            
+    def __len__(self):
+        return len(self.images)
