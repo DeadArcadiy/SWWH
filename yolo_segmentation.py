a0ed1372d57eb0b7db57f63ecffed49211ce0948
Denis Borychev <denisborycev@gmail.com>
2024-02-17 22:59:36 +0300

    testing yolo

--- yolo_segmentation.py
+++ yolo_segmentation.py
@@ -3,9 +3,9 @@ import torch
 # Load a model
 if __name__ == '__main__':
     print(torch.cuda.is_available())
-    model = YOLO("yolov8n.yaml")  # build a new model from scratch
-    model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-    model.eval()
-# Use the model
-    model.train(data='train.yaml', epochs=50, batch=4, workers=4, patience=10)  # train the model
-    metrics = model.val()  # evaluate model performance on the validation set
+    model = YOLO('yolov8n-seg.yaml')  # build a new model from YAML
+    model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
+    model = YOLO('yolov8n-seg.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+
+    model.train(data='train.yaml', epochs=100, batch=4, patience=10)  # train the model
+    #metrics = model.val()  # evaluate model performance on the validation set
