b3f3d96c6e62fe9a17596862a7ee0015264d7335
Denis Borychev <denisborycev@gmail.com>
2024-03-31 14:52:56 +0300

    test segmentation

--- dataset.py
+++ dataset.py
@@ -5,13 +5,14 @@ from tqdm import tqdm
 import os
 
 class Train_dataset(torch.utils.data.Dataset):
-    def __init__(self,height,augmentations,image_folder,mask_folder,device):
+    def __init__(self,height,augmentations,image_folder,mask_folder,device,preprocess_input):
         self.image_folder = image_folder
         self.height = height
         self.mask_folder = mask_folder
         self.transforms = augmentations
         self.images = os.listdir(image_folder)
         self.device = device
+        self.preprocess_input = preprocess_input
         print(self.images)
 
     def __getitem__(self,i):
@@ -22,11 +23,15 @@ class Train_dataset(torch.utils.data.Dataset):
         transformed_image = transformed['image']
         transformed_mask = transformed['mask']
         transformed_mask = transformed_mask.reshape(self.height,self.height,1)
+        if self.preprocess_input:
+            transformed_image = self.preprocess_input(transformed_image)
+        ########################################################
         transformed_image = torch.Tensor(transformed_image)
         transformed_mask = torch.Tensor(transformed_mask)
+        print(transformed_mask.shape)
+        ############################################
         transformed_image = transformed_image.permute((2, 0, 1))
         transformed_mask = transformed_mask.permute((2, 0, 1))
-        transformed_image /= 255
         return transformed_image.to(self.device),transformed_mask.to(self.device)
             
             
@@ -34,7 +39,7 @@ class Train_dataset(torch.utils.data.Dataset):
         return len(self.images)
 
 class Test_dataset(torch.utils.data.Dataset):
-    def __init__(self,augmentations,image_folder,device):
+    def __init__(self,augmentations,image_folder,device,preprocess_input):
         self.image_folder = image_folder
         self.transforms = augmentations
         self.images = os.listdir(image_folder)
